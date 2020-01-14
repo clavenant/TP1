9ca5e0efcae4b4cacd8acc94b149369dcf62bc1e
Cédric Lavenant <clavenant@gmail.com>
2020-01-14 15:58:02 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,7 @@
 ###application.bat - 201x - Cédric Lavenant ###
 echo "version 1.1"
 echo "Copyright company - 201x"
+"Build : 10:00"###application.bat - 201x - Cédric Lavenant ###
+echo "version 1.1"
+echo "Copyright company - 201x"
 "Build : 10:00"
